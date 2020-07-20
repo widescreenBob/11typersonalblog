a5bbf42e9bc82c6b077b96b8239b4cdcfe9817b6
Tobias Williams <tobias.williams@mediacurrent.com>
2020-07-19 21:48:03 -0400

    11ty Cypress tests

--- cypress/integration/social_test.js
+++ cypress/integration/social_test.js
@@ -1,8 +1,50 @@
-describe('social links test', () => {
-  it('Visits the Homepage', () => {
+
+
+describe('social links test', function () {
+  beforeEach(function () {
     cy.visit('https://widescreenbob.com/')
-    cy.contains('Read Tobias\' tweets').click()
-    //cy.get('.header__social-item')
-    //cy.url().should('include', '/twitter.com')
   })
-})
+
+  context('testing the twitter', function () {
+    it('Checks to see if the twitter link contains the base url', function () {
+      cy.get('.twitter')
+        // the href 'attribute' will only ever be what the
+        // literal value is on the element itself and will
+        // match what was served by the <html> payload
+        .should('have.attr', 'href')
+        .and('include', 'twitter.com')
+    })
+  })
+
+  context('testing the github', function () {
+    it('Checks to see if the github link contains the base url for the site', function () {
+      cy.get('.github')
+        // the href 'attribute' will only ever be what the
+        // literal value is on the element itself and will
+        // match what was served by the <html> payload
+        .should('have.attr', 'href')
+        .and('include', 'github.com')
+    })
+  })
+
+  context('testing the drupal', function () {
+    it('Checks to see if the drupal link contains the base url for the site', function () {
+      cy.get('.drupal')
+        // the href 'attribute' will only ever be what the
+        // literal value is on the element itself and will
+        // match what was served by the <html> payload
+        .should('have.attr', 'href')
+        .and('include', 'drupal.org')
+    })
+  })
+
+  context('testing the codepen', function () {
+    it('Checks to see if the codepen link contains the base url for the site', function () {
+      cy.get('.codepen')
+        // the href 'attribute' will only ever be what the
+        // literal value is on the element itself and will
+        // match what was served by the <html> payload
+        .should('have.attr', 'href')
+        .and('include', 'codepen.io')
+    })
+  })

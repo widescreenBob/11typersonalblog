f80fe3331ab130ac0db0983cd4f4d168de97b698
Tobias Williams <tobias.williams@mediacurrent.com>
2020-07-19 22:00:47 -0400

    Remove the sample test.

--- cypress/integration/sample_specs.js
+++
@@ -1,7 +0,0 @@
-describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
-    cy.visit('https://widescreenbob.com/')
-    cy.contains('Creating this site with eleventy').click()
-    cy.url().should('include', '/blog-posts/')
-  })
-})
